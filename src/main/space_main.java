6ff5a4aebe8d7813905e5f3f95635844a11ac260
lil-rick3 <83485898+lil-rick3@users.noreply.github.com>
2022-11-13 04:19:39 -0600

    Add files via upload

---
+++ src/main/space_main.java
@@ -0,0 +1,10 @@
+package main;
+import graphics.Space_Frame;
+
+
+public class space_main {
+	public static void main(String[] args) {
+		Space_Frame thing = new Space_Frame();
+		thing.game_gui.run();
+	}
+}
